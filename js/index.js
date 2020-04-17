ae1e56ba0a580a592dd669c1f0348275dd6b1b37
yifei520 <2318426929@qq.com>
2020-04-17 11:28:19 +0800

    静态响应式完善JS功能

--- js/index.js
+++ js/index.js
@@ -1,79 +1,188 @@
-//方法一  当页面小于980时不满足会变形飘起来影响布局
-
+////使用原生JS
 // window.onload = function(){
+//     //个人信息与侧边菜单吸顶
+//     (function(){
+//         let oContainer = document.getElementById("container");
+//         let oPer = document.querySelectorAll(".personalall");    //获取左侧个人信息节点 
+//         let oPerTop = oPer[0].offsetTop;       //获取左侧距离顶部高度
+//         // console.log(oPerTop);
+//         window.onscroll = (function move(){
+//             //获取滚动条实时高度
+//             let backTop1 = document.documentElement.scrollTop || document.body.scrollTop;  
+//             console.log(backTop1); 
+//             if( oPerTop - backTop1 < 15){
+//                 oContainer.className = "fixed";
+//             }else{
+//                 oContainer.className = "";
+//                 // oContainer.classList.remove("fixed");   
+//             }
+//             return move;
+//         })();        //立即执行解决滚动在下面时刷新两侧不固定  
+//     })();
 
-//     //找到左边个人信息区id
-//     var per = document.getElementsByClassName("personalall")[0];
-//     //获取左侧距离窗口顶部距离
-//     var perTop = per.offsetTop;
-//     // console.log(perTop);
-
-//     //找到右边的侧边栏
-//     var Bar = document.getElementsByClassName("sideNav")[0];
-//     var BarTop = Bar.offsetTop;
-//     // console.log(BarTop);
-
-// //     制作吸顶作用
-//     window.onscroll = function(){
-
-//         var backTop=document.documentElement.scrollTop ||document.body.scrollTop;
-
-//         // console.log(backTop);
-
-//         if( backTop - perTop > -15){
-//             per.style.position = "fixed"
-//             per.style.top = "15"+"px";
-//             // per.style.zIndex = "100";
-//         }
-//         else{
-//             per.style.position = "";
-//           }
-
-//         if( backTop - BarTop > -15){
-//             Bar.style.position = "fixed"
-//             Bar.style.top = "15"+"px";
-//             //Bar.style.zIndex = "100";
-//         }
-//         else{
-//             Bar.style.position = "";
-//         }
-//     }
+//     //侧边菜单点击事件
+//     (function(){
+//         let oLi = document.querySelectorAll("#navBar .sideNav li");
+//         let oSection = document.querySelectorAll(".con-part");
+//         let timer = null; 
+//         let isbool = true; 
+//         oLi.forEach((node,i)=>{
+//             node.onclick = function(){
+//                 // console.log(i);
+//                 clearInterval(timer);
+//                 let toTop = oSection[i].offsetTop;    //获取每个层距的文档高度
+//                 console.log(toTop);
+//                 //给对应的li标签有active名字，其他的去掉active名字
+//                 oLi.forEach((ele)=>{
+//                     ele.className = "";
+//                 });
+//                 node.classList.add("active");
+//                 //实现点击缓慢滚动效果
+//                 timer = setInterval(() => {
+//                     let backTop = document.documentElement.scrollTop || document.body.scrollTop;
+//                     console.log(backTop);
+//                     //设置滚动的步长并减小
+//                     var speed = Math.floor((backTop - toTop)/5);    
+//                     // 阻止滚动事件
+//                     isbool = true;
+//                     document.documentElement.scrollTop = backTop - speed; 
+//                     if( Math.abs(backTop - toTop)<5 ){     //控制清除定时器
+//                         clearInterval(timer);
+//                     }
+//                 }, 30);
+//                 window.onload = function(){
+//                     window.onscroll = function(){
+//                         if(!isbool){
+//                             clearInterval(timer);
+//                         }else{
+//                             isbool = false;
+//                         }
+//                     }  
+//                 }      
+//             }
+//         });
+//     })();
 // }
 
+//     //滚动改变名字样式改变
+//     (function(){
+//         let oLi = document.querySelectorAll("#navBar .sideNav li");
+//         let oSection = document.querySelectorAll(".con-part");
+//         // window.onscroll = (function move1(){
+//         //     let sTop = document.documentElement.scrollTop || document.body.scrollTop;
+//         //     // console.log(sTop);
+//         //     let len = oSection.length;
+//         //     let index;
+//         //     for(var i=0;i<len;i++){
+//         //         let toTop2 = oSection[i].offsetTop;
+//         //         // console.log(toTop);
+//         //         if( toTop2  -  sTop > 150){
+//         //             index = i - 1;
+//         //             break;
+//         //         }
+//         //     }
+//         //     index === undefined && (index = len - 1);
+//         //     index = Math.max(index,0);
+//         //     // console.log(index);
+//         //     oLi.forEach((node)=>{
+//         //         node.className = "";
+//         //     });
+//         //     oLi[index].className = "active"  
+//         //     return move1;
+//         // })();
+//     })();
+// };
 
 
 
-//方法二
-
+//使用JQ写
 $(function () {
-    /*
-    * personal + sideNav
-    * 个人信息与侧边菜单吸顶
-    */
+    //个人信息与侧边菜单吸顶
     (function(){
-      var $personal = $("#per");
-    //   console.log($personal);
-      var $personalMain = $("#per .personalall");
-    //   console.log($personalMain);  
-    //  var $navBar = $("#navBar");
-    //   console.log($sideNav);   
-      var $sideNav = $("#navBar .sideNav");
+        var $personal = $("#per");
+        var $personalMain = $("#per .personalall");
+        // var $navBar = $("#navBar");
+        // console.log($sideNav);   
+        var $sideNav = $("#navBar .sideNav");
 
-      $(window).scroll((function m() {
+        $(window).scroll((function move() {
+            if (($personal.offset().top - $(document).scrollTop()) <= 10){
+                $personalMain.addClass("fixed");
+                $sideNav.addClass("fixed");
 
-        //   var dd = $personal.offset().top;
-        //   console.log(dd);
+            }else{
+                $personalMain.removeClass("fixed");
+                $sideNav.removeClass("fixed");
+            }
+            return move;
+        })());
 
-        if (($personal.offset().top - $(document).scrollTop()) <= 10){
+    })();
+    //侧边菜单点击事件
+    (function(){
+        let $li = $("#navBar .sideNav li");
+        let $oSection = $(".con-part");
+        let len = $oSection.length;
+        //点击事件
+        $li.click(function(){
+            let i = $(this).index();    //存储点击索引
+            let toTop = $oSection.eq(i).offset().top;
+            //点击滚动效果
+            $("html")
+                .stop()
+                .animate({scrollTop : toTop},300);
+            // 点击改变样式
+            $(this)
+                .addClass("active")
+                .siblings()
+                .removeClass("active");
+        });
+        //滚动时改变名字
+        // $(window).scroll(function(){
+        //     let sTop = $(window).scrollTop();
+        //     let index;
+        //     for(let i=0;i<len;i++){
+        //         let toTop = $oSection.eq(i).offset().top;  //获取每层距离顶部高度
+        //         if( toTop - sTop > 150){
+        //             index = i - 1;
+        //             break;
+        //         }
+        //     }
+        //     index === undefined && (index = len -1);
+        //     index = Math.max(index,0);
+        //     console.log(index);
+        //     $li.eq(index) 
+        //         .addClass("active")
+        //         .siblings()
+        //         .removeClass("active");        
+        // });
+        
 
-          $personalMain.addClass("fixed");
-          $sideNav.addClass("fixed");
-
-        }else{
-          $personalMain.removeClass("fixed");
-          $sideNav.removeClass("fixed");
-        }
-        return m;
-      })());
+        //滚动时改变名字
+        function move(){
+            let sTop = $(window).scrollTop();
+            let index;
+            for(let i=0;i<len;i++){
+                let toTop = $oSection.eq(i).offset().top;  //获取每层距离顶部高度
+                if( toTop - sTop > 150){
+                    index = i - 1;
+                    break;
+                }
+            }
+            index === undefined && (index = len -1);
+            index = Math.max(index,0);
+            console.log(index);
+            $li.eq(index) 
+                .addClass("active")
+                .siblings()
+                .removeClass("active");        
+        };
+        move();
+        //节流 --  持续性事件触发时
+        let timer = null;
+        $(window).scroll(function(){
+            clearTimeout(timer);
+            timer = setTimeout(move,200);
+        });
     })();
 });
